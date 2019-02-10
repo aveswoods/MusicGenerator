808c402514d7de04f67afe13b3e1206ae21d476d
andrewdecker <47485012+andrewdecker@users.noreply.github.com>
2019-02-10 01:02:42 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -23,15 +23,18 @@ public class Driver {
 							+ "4. Guitar\n"
 							+ "5. Ensemble\n"
 							+ "6. Piano\n"
-							+ "7. Percussion\n");
+							+ "7. Percussion");
 		int genre = scan.nextInt();
+		MusicName musicName = new MusicName();
+		String title = musicName.makeTitle();
+		System.out.println("Now listening to: " + "\"" + title + "\"");
 		System.out.println("Like what you hear? You can find the MIDI file in the same directory as this program!");
 		Random random = new Random();
 		int bpm = random.nextInt(61) + 60;
 		int i1 = ins.randomInstrument(genre);
 		int i2 = ins.randomInstrument(genre);
 		WriteRandomMidi.writeMidi(Generator.play(i1, i2, bpm),
-									bpm, "GeneratedMidi.midi");
+									bpm, title + ".midi");
 		
 	}
 
